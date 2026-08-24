355f6390f5202ab3b36753a26b1d6d4cfd75e191
Charlie <meow@charliecat.space>
2026-08-24 13:42:54 +0200

    use overpass-api.de for osm data

--- fetch/script.sh
+++ fetch/script.sh
@@ -10,7 +10,7 @@ unzip -o ./tmp/feed.zip -d ./tmp/feed
 rm ./tmp/feed.zip
 
 ## fetch openstreetmap data
-curl -fsSL -A "$UA" https://overpass.kumi.systems/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea%28id%3A3600014296%29-%3E.searchArea%3B%28nwr%5B%22railway%22%3D%22station%22%5D%28area.searchArea%29%3Bnwr%5B%22railway%22%3D%22halt%22%5D%28area.searchArea%29%3B%29%3Bout%20geom%3B > ./tmp/OSMStops.json
+curl -fsSL -A "$UA" https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea%28id%3A3600014296%29-%3E.searchArea%3B%28nwr%5B%22railway%22%3D%22station%22%5D%28area.searchArea%29%3Bnwr%5B%22railway%22%3D%22halt%22%5D%28area.searchArea%29%3B%29%3Bout%20geom%3B > ./tmp/OSMStops.json
 
 mkdir -p ./data
 
